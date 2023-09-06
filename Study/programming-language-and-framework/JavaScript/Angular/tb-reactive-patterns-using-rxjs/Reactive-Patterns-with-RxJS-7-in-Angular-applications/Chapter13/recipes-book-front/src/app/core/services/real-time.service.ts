0a00c96f06047a2b348412ea0398c4f98f1ee300
wisnuprsj <wisnuprsj@gmail.com>
2023-09-07 01:14:31 +0700

    study rxjs

---
+++ Study/programming-language-and-framework/JavaScript/Angular/tb-reactive-patterns-using-rxjs/Reactive-Patterns-with-RxJS-7-in-Angular-applications/Chapter13/recipes-book-front/src/app/core/services/real-time.service.ts
@@ -0,0 +1,61 @@
+import { Injectable } from '@angular/core';
+import { webSocket, WebSocketSubject } from 'rxjs/webSocket';
+import { environment } from '../../../environments/environment';
+import { Observable, timer, EMPTY, BehaviorSubject } from 'rxjs';
+import { retryWhen, tap, delayWhen, switchAll, catchError } from 'rxjs/operators';
+import { Message } from '../model/message.model';
+export const WS_ENDPOINT = environment.wsEndpoint;
+export const RECONNECT_INTERVAL = environment.reconnectInterval;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RealTimeService {
+
+  private socket$!: WebSocketSubject<Message> | undefined;
+  private messagesSubject$ = new BehaviorSubject<Observable<Message>>(EMPTY);
+  public messages$ = this.messagesSubject$.pipe(switchAll(), catchError(e => { throw e }));
+
+  public getNewWebSocket(): WebSocketSubject<Message> {
+    return webSocket({
+      url: WS_ENDPOINT,
+      closeObserver: {
+        next: () => {
+          console.log('[DataService]: connection closed');
+          this.socket$ = undefined;
+          this.connect({ reconnect: true });
+        }
+      },
+    })
+  }
+
+
+
+  sendMessage(msg: Message) {
+    this.socket$?.next(msg);
+  }
+  close() {
+    this.socket$?.complete();
+  }
+
+  public connect(cfg: { reconnect: boolean } = { reconnect: false }): void {
+
+    if (!this.socket$ || this.socket$.closed) {
+      this.socket$ = this.getNewWebSocket();
+      const messages = this.socket$.pipe(cfg.reconnect ? this.reconnect : o => o,
+        tap({
+          error: error => console.log(error),
+        }), catchError(_ => EMPTY));
+      this.messagesSubject$.next(messages);
+    }
+  }
+
+
+
+  private reconnect(observable: Observable<Message>): Observable<Message> {
+    return observable.pipe(retryWhen(errors => errors.pipe(tap(val => console.log('[Data Service] Try to reconnect', val)),
+      delayWhen(_ => timer(RECONNECT_INTERVAL)))));
+  }
+
+
+}
---
+++ Study/programming-language-and-framework/JavaScript/Angular/tb-reactive-patterns-using-rxjs/Reactive-Patterns-with-RxJS-7-in-Angular-applications/Chapter13/recipes-book-front/src/app/core/services/shared-data.service.spec.ts
@@ -0,0 +1,12 @@
+import { TestBed } from '@angular/core/testing';
+import { SharedDataService } from './shared-data.service';
+
+describe('SharedDataService', () => {
+  let service: SharedDataService;
+
+  beforeEach(() => {
+    service = TestBed.inject(SharedDataService);
+  });
+
+});
+
