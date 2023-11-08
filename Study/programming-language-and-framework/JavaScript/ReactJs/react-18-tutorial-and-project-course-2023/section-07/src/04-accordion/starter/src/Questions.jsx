a4f5d2b3cf979f11360919a0ae53a4076fa73d58
Wisnu P W <wisnuprsj@gmail.com>
2023-11-08 22:02:08 +0700

    finished section 7 on questions projects

---
+++ Study/programming-language-and-framework/JavaScript/ReactJs/react-18-tutorial-and-project-course-2023/section-07/src/04-accordion/starter/src/Questions.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { SingleQuestion } from "./SingleQuestion";
+
+export const Questions = ({ questions }) => {
+  return (
+    <section className="container">
+      <h1>Questions</h1>
+      {questions.map((question) => {
+        return <SingleQuestion key={question.id} {...question} />;
+      })}
+    </section>
+  );
+};
--- Study/programming-language-and-framework/JavaScript/ReactJs/react-18-tutorial-and-project-course-2023/section-07/src/main.jsx
+++ Study/programming-language-and-framework/JavaScript/ReactJs/react-18-tutorial-and-project-course-2023/section-07/src/main.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import App from "./03-reviews/starter/src/App";
-import "./03-reviews/starter/src/index.css";
+import App from "./04-accordion/starter/src/App";
+import "./04-accordion/starter/src/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
