a937b036d153a1131ef0c9596e7075b99a301114
Wisnu P W <wisnuprsj@gmail.com>
2023-12-24 15:16:10 +0700

    refactoring folders and studying react-native

---
+++ Study/programming-language-and-framework/mobile-development/react-native-the-practical-guide/RNCourse/App.js
@@ -0,0 +1,33 @@
+// import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.appContainer}>
+      <View style={styles.goalInputContainer}>
+        <TextInput style={styles.textInput} placeholder="Your course goal!" />
+        <Button title="Add Goal" />
+      </View>
+      <View>
+        <Text>List of goals ... </Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  appContainer: {
+    padding: 50,
+  },
+  goalInputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#cccccc",
+    width: "80%",
+    marginRight: 8,
+    padding: 8,
+  },
+});
